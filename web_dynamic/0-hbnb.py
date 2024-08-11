5eb8c350fecd5d5fa8b3b1bffd872d5d8ac19653
Rachid Boukind <boukind.r1@@gmail.com>
2024-08-11 22:46:49 +0000

    a

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from uuid import uuid4
+
+app = Flask(name)
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """
+    Renders the main HBnB filters page
+    """
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    places = storage.all("Place").values()
+    cache_id = uuid4()
+    return render_template('0-hbnb.html', 
+                           states=states, 
+                           amenities=amenities, 
+                           places=places, 
+                           cache_id=cache_id)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Closes the database session after each request
+    """
+    storage.close()
+
+if name == "main":
+    app.run(host='0.0.0.0', port=5000)
